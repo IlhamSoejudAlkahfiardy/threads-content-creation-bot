5f7edd06783e1befb2a496d279a2624a2dd91f99
IlhamSoejudAlkahfiardy <ilhamyusuf386@gmail.com>
2026-09-10 13:10:32 +0700

    feat: add predefined topics configuration and lookup utility for Meta Threads

--- src/config/topics.js
+++ src/config/topics.js
@@ -14,6 +14,7 @@ const TOPICS = [
   { id: "beauty", label: "💄 Beauty Threads", tag: "beautythreads" },
   { id: "books", label: "📚 Book Threads", tag: "bookthreads" },
   { id: "cats", label: "🐱 Cats of Threads", tag: "catsofthreads" },
+  { id: "beauty", label: "Beauty Threads", tag: "beautythreads" },
 ];
 
 /**
